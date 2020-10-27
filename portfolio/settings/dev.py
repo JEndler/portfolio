e9a755b153ba8830934271830f81172abce66249
Jakob Endler <jakob.endler@hotmail.de>
2020-10-27 20:36:50 +0100

    Inital Commit for Wagtail Setup

---
+++ portfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '1e)_a&7*23=-83ig9m@i-(@)x33#j_2hrb$n#ca5*zbj15_7#2'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
