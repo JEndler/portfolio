24f9b5708b3f8cc890db1dbfc1574f1ba0d055d4
Jakob Endler <jakob.endler@hotmail.de>
2020-11-04 23:13:35 +0100

    Changed Debug Setting

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -16,7 +16,7 @@ MEDIA_URL = '/media/'
 
 WAGTAIL_SITE_NAME = 'Jakobs Portfolio'
 
-DEBUG = True
+DEBUG = False
 
 with open('/home/projects/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
