8893ccce17a7f2822d8c818007f91984bdf5e952
Jakob Endler <jakob.endler@hotmail.de>
2020-11-06 14:07:51 +0100

    Fixed initial Production Setup

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,13 +1,6 @@
 from .base import *
 
-DEBUG = False
-
-try:
-    from .local import *
-except ImportError:
-    pass
-
-
+ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -15,3 +8,28 @@ MEDIA_URL = '/media/'
 
 WAGTAIL_SITE_NAME = 'Jakobs Portfolio'
 
+DEBUG = False
+
+with open('/home/projects/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log')
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
